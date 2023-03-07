7a656c4328a23103372a7c0f7f32c9bb1d80efea
NCIGareth <x19122748@student.ncirl.ie>
2023-03-07 10:39:29 +0000

    Deployment

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
-sudo apt update && sudo apt install npm
+sudo apt update && sudo apt install nodejs npm
 sudo npm install -g pm2
-pm2 stop mathapp
+pm2 stop SimpleApplication
 cd SimpleApplication/
 npm install
-pm2 start app.js
+pm2 start ./bin/www --name SimpleApplication --env=production
